54d379f7dc5cf07004374381c77ea20b0434a4ce
Guilherme Santos <sntguilherme@gmail.com>
2020-02-13 23:48:22 -0300

    Chat screen with the ChatMessage and with the layout colors

--- frontend/src/Chat/Chat.js
+++ frontend/src/Chat/Chat.js
@@ -3,53 +3,70 @@ import styled from 'styled-components';
 import IconButton from '@material-ui/core/IconButton';
 import TextField from '@material-ui/core/TextField';
 import SendRoundedIcon from '@material-ui/icons/SendRounded';
-import Typography from '@material-ui/core/Typography';
 
 import Screen from '../commons/components/Screen/Screen';
+import ChatMessage from '../commons/components/ChatMessage/ChatMessage';
 
 const SendMessage = styled.div`
   display: flex;
   justify-content: space-between;
 `;
 
-const Message = styled.div`
-  display: flex;
-`;
-
-const MessageContent = styled.div`
-  flex: 1;
-`;
-
-const MessageText = styled.p`
-
-`;
-
-const MessageUser = styled.span`
-
-`;
-
-const MessageTime = styled.span`
-  display: flex;
-  align-items: flex-end;
+const StyledTextField = styled(TextField)`
+  background-color: #fff;
 `;
 
 export default function Chat() {
+  const messages = [
+    {
+      id: 1,
+      userName: 'Guest',
+      message: 'Want to bang tonight?',
+      date: '12/01/2020',
+    },
+    {
+      id: 2,
+      userName: 'Guest',
+      message: 'Want to bang tonight?',
+      date: '12/01/2020',
+    },
+    {
+      id: 3,
+      userName: 'Guest',
+      message: 'Want to bang tonight?',
+      date: '12/01/2020',
+    },
+    {
+      id: 4,
+      userName: 'Guest',
+      message: 'Want to bang tonight?',
+      date: '12/01/2020',
+    }
+  ];
+  // TODO: ul e li
   return (
     <Screen>
-      {({ Content, Footer }) => (
+      {({Content, Footer}) => (
         <>
-          <Content>
-            <Message>
-              <MessageContent>
-                <Typography variant="subtitle2" component={MessageUser}>Guest</Typography>
-                <Typography variant="body2" component={MessageText}>Want to bang tonight?</Typography>
-              </MessageContent>
-              <Typography variant="caption" component={MessageTime}>12/01/2020</Typography>
-            </Message>
+          <Content backgroundColor="#E5DDD5">
+            {messages.map(({id, userName, message, date}) => (
+              <ChatMessage
+                key={id}
+                userName={userName}
+                message={message}
+                dateTime={date}
+              />
+            ))}
+            <ChatMessage
+              align="left"
+              backgroundColor="#DCF8C6"
+              message="What?"
+              dateTime="25/05/1992"
+            />
           </Content>
-          <Footer>
+          <Footer backgroundColor="#f0f0f0">
             <SendMessage>
-              <TextField id="filled-basic" placeholder="Enter message" variant="outlined" fullWidth/>
+              <StyledTextField id="filled-basic" placeholder="Enter message" variant="outlined" fullWidth/>
               <IconButton aria-label="delete">
                 <SendRoundedIcon/>
               </IconButton>
