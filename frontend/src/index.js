1c47bf18044e6a91eaae25565eb3e93c4b2cf9c4
Guilherme Santos <sntguilherme@gmail.com>
2020-02-15 17:39:10 -0300

    Refactoring App components structure

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
-import App from './App';
+import App from 'app/App';
+
 import * as serviceWorker from './serviceWorker';
 import configureStore from './redux/store';
 
