f4c950f5c161f3f35e6cef1d8a5e04d8e6a58d16
Guilherme Santos <sntguilherme@gmail.com>
2020-02-16 12:54:11 -0300

    Tabs from App component moved to AppTabs component

---
+++ frontend/src/app/components/AppTabs/AppTabs.js
@@ -0,0 +1,67 @@
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Badge from '@material-ui/core/Badge';
+import React, {useCallback} from 'react';
+import styled from 'styled-components';
+import AppBar from '@material-ui/core/AppBar';
+import {useDispatch, useSelector} from 'react-redux';
+
+import Chat from 'features/Chat/Chat';
+import Settings from 'features/Settings/Settings';
+import {actions as messagesActions, selectors as messagesSelectors} from 'redux/modules/messages';
+
+const TabsContent = styled.div`
+  height: 100%;
+
+  display: grid;
+  grid-template-rows: min-content auto;
+`;
+
+const Body = styled.div`
+  overflow-y: auto;
+`;
+const tabs = {
+  0: <Chat/>,
+  1: <Settings/>,
+};
+
+function AppTabs() {
+  const dispatch = useDispatch();
+  const unreadMessages = useSelector(messagesSelectors.getUnreadMessages);
+
+  const [activeTab, setActiveTab] = React.useState(0);
+
+  const isChatTabActive = () => {
+    return activeTab === 0;
+  };
+
+  const resetUnreadMessages = useCallback(
+    () => dispatch(messagesActions.resetUnreadMessages()),
+    [dispatch]
+  );
+
+  const handleChangeTab = (event, newValue) => {
+    setActiveTab(newValue);
+    resetUnreadMessages();
+  };
+
+  return (
+    <TabsContent>
+      <AppBar position="static">
+        <Tabs value={activeTab} onChange={handleChangeTab}>
+          <Tab label={
+            <Badge badgeContent={isChatTabActive ? 0 : unreadMessages} color="secondary">
+              Chat
+            </Badge>
+          }/>
+          <Tab label="Settings"/>
+        </Tabs>
+      </AppBar>
+      <Body>
+        {tabs[activeTab]}
+      </Body>
+    </TabsContent>
+  );
+}
+
+export default AppTabs;
