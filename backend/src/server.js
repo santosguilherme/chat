3e85681e0bb3ae13a7dd14cbc09b6ccedc966f14
Guilherme Santos <sntguilherme@gmail.com>
2020-02-16 23:18:45 -0300

    Refactored websocket server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,49 +1,31 @@
 import express from 'express';
-import http from 'http';
 import socketIO from 'socket.io';
 import uuid from 'uuid';
 
-
 const app = express();
-
-// const server = http.createServer(app);
-
 const server = app.listen(8080);
-
 const io = socketIO.listen(server);
 
-const messages = [];
-
-io.sockets.on('connection', async (socket) => {
-  console.log('socket connected:', socket.id);
-
+io.sockets.on('connection', async socket => {
   socket.emit('chat.join', socket.id);
 
-  socket.on('chat.send', ({ userName, text}) => {
-
+  socket.on('chat.send', ({ userName, text }) => {
     const message = {
       id: uuid.v4(),
       text,
       userId: socket.id,
       userName,
-      dateTime: new Date().toJSON()
+      dateTime: new Date().toJSON(),
     };
-
-    messages.push(message);
 
     io.sockets.emit('chat.receive', message);
   });
 
-  socket.on('disconnect', function () {
+  socket.on('disconnect', () => {
     console.log('socket disconnect:', socket.id);
   });
 
-  socket.on('error', function (err) {
-    console.log('received error from socket:', socket.id);
-  })
+  socket.on('error', err => {
+    console.log('received error from socket:', socket.id, err);
+  });
 });
-
-
-// app.listen(4123,() => {
-//     console.log("Server is running in port 4123");
-// })
