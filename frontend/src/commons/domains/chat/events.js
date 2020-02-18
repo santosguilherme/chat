bdb1e65aa63f42f13485ee68107bcce3dc547eb1
Guilherme Santos <sntguilherme@gmail.com>
2020-02-18 10:51:40 -0300

    Extracting websocket events to constants

---
+++ frontend/src/commons/domains/chat/events.js
@@ -0,0 +1,7 @@
+export const CONNECTED = 'connect';
+
+export const JOINED_CHAT = 'chat.join';
+
+export const RECEIVED_MESSAGE = 'chat.receive';
+
+export const SEND_MESSAGE = 'chat.send';
--- frontend/src/redux/sagas/chat.js
+++ frontend/src/redux/sagas/chat.js
@@ -4,6 +4,10 @@ import {
 import { eventChannel } from 'redux-saga';
 import io from 'socket.io-client';
 
+import {
+  CONNECTED, JOINED_CHAT, RECEIVED_MESSAGE, SEND_MESSAGE,
+} from 'commons/domains/chat/events';
+
 import { actions } from '../modules/messages';
 import { actions as appActions } from '../modules/app';
 
@@ -12,7 +16,7 @@ function* write(socket) {
     const { payload } = yield take(actions.sendMessage);
     const { userName, text } = payload;
 
-    socket.emit('chat.send', {
+    socket.emit(SEND_MESSAGE, {
       userName,
       text,
     });
@@ -24,7 +28,7 @@ function connect() {
   const socket = io(`${protocol}//${hostname}:8080`);
 
   return new Promise(resolve => {
-    socket.on('connect', () => {
+    socket.on(CONNECTED, () => {
       resolve(socket);
     });
   });
@@ -34,7 +38,7 @@ export function subscribe(socket) {
   return eventChannel(emit => {
     const update = message => emit(actions.updateMessages(message));
 
-    socket.on('chat.receive', update);
+    socket.on(RECEIVED_MESSAGE, update);
 
     return () => {};
   });
@@ -54,7 +58,7 @@ function join(socket) {
   return eventChannel(emit => {
     const update = id => emit(actions.chatJoin(id));
 
-    socket.on('chat.join', update);
+    socket.on(JOINED_CHAT, update);
 
     return () => {};
   });
